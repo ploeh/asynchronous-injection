9652eec251a3dcea7c6241afb4c2d8018b2e051a
Mark Seemann <mark@ploeh.dk>
2018-08-09 10:02:23 +0200

    Add Maybe class

---
+++ BookingApi/Maybe.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ploeh.Samples.BookingApi
+{
+    public sealed class Maybe<T>
+    {
+        private readonly bool hasItem;
+        private readonly T item;
+
+        public Maybe()
+        {
+            hasItem = false;
+        }
+
+        public Maybe(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+
+            hasItem = true;
+            this.item = item;
+        }
+
+        public TResult Match<TResult>(TResult nothing, Func<T, TResult> just)
+        {
+            if (nothing == null)
+                throw new ArgumentNullException(nameof(nothing));
+            if (just == null)
+                throw new ArgumentNullException(nameof(just));
+
+            return hasItem ? just(item) : nothing;
+        }
+
+        public Maybe<TResult> Select<TResult>(Func<T, TResult> selector)
+        {
+            return Match(
+                nothing: new Maybe<TResult>(),
+                just: x => new Maybe<TResult>(selector(x)));
+        }
+
+        public Maybe<TResult> SelectMany<TResult>(
+            Func<T, Maybe<TResult>> selector)
+        {
+            return Match(nothing: new Maybe<TResult>(), just: selector);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Maybe<T> other))
+                return false;
+
+            return Match(
+                nothing: !other.hasItem,
+                just: x => other.Match(
+                    nothing: !hasItem,
+                    just: y => Equals(x, y)));
+
+        }
+
+        public override int GetHashCode()
+        {
+            return Match(nothing: 0, just: x => x.GetHashCode());
+        }
+    }
+}
